ab60e7c1844578c4ea0766e6b62e53dfe08388ce
Luigi Pertoldi <luigi.pertoldi@pd.infn.it>
2018-05-08 13:13:27 +0200

    style fixes for bat.rb

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -1,23 +1,21 @@
 class Bat < Formula
   desc "Bayesian analysis toolkit"
-  homepage "http://mpp.mpg.de/bat"
+  homepage "https://bat.mpp.mpg.de/"
   url "https://github.com/bat/bat/releases/download/v1.0.0-RC2/BAT-1.0.0-RC2.tar.gz"
   sha256 "51267cb1851d56858c44568a3029047ad9a3f76fbe2d58872bc78b7610d225b6"
 
   depends_on "root"
   depends_on "cuba" => :recommended
 
-  option "multithreading", "Enable OpenMP multithreading support (gcc only)" if OS.linux?
+  option "with-multithreading", "Enable OpenMP multithreading support (gcc only)" if OS.linux?
 
   def install
-    opts = [
-      "--prefix=#{prefix}"
-    ]
+    opts = ["--prefix=#{prefix}"]
 
     if build.without? "cuba"
-        opts << "--with-cuba=no"
+      opts << "--with-cuba=no"
     else
-        opts << "--with-cuba"
+      opts << "--with-cuba"
     end
 
     if build.with?("multithreading") && OS.mac?
@@ -28,12 +26,12 @@ class Bat < Formula
 
     system "./configure", *opts
     system "make", "install"
-    (share/"BAT/examples").install Dir["examples/basic"]
-    (share/"BAT/examples").install Dir["examples/advanced"]
-    (share/"BAT").install Dir["doc"]
+    (share/"BAT/examples").install "examples/basic"
+    (share/"BAT/examples").install "examples/advanced"
+    (share/"BAT").install "doc"
   end
 
   test do
-      system "#{prefix}/share/BAT/examples/basic/binomial/runEfficiency"
+    system "#{share}/BAT/examples/basic/binomial/runEfficiency"
   end
 end
