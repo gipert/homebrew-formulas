d5bd958362b3c1fb658cc60569888a368c522444
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2019-06-03 22:48:36 +0200

    [vim] update

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -2,8 +2,8 @@ class Vim < Formula
   desc "Vi 'workalike' with many additional features"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.1.0650.tar.gz"
-  sha256 "f734c283604f5889b3f62fec045b59f70b804165041370f2da1cbf8b0e0b4d9f"
+  url "https://github.com/vim/vim/archive/v8.1.1450.tar.gz"
+  sha256 "bf7837109b37d4426fd575a8f04e11f6485fabb072b799811cf868e400720490"
   head "https://github.com/vim/vim.git"
 
   depends_on "gettext"
